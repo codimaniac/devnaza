94d9d2a8809b26cd5fe1a0f25fa38e3279a53799
Codimaniac <simplyanthony007@gmail.com>
2025-11-02 00:46:02 +0100

    Fix: fixed mobile nav

--- src/components/ui/navlinks/NavLinks.jsx
+++ src/components/ui/navlinks/NavLinks.jsx
@@ -4,15 +4,15 @@ import SocialLinks from '../social-links/SocialLinks'
 
 const NavLinks = ({ isOpen, toggleNav }) => {
   return (
-      <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+      <ul className={`nav-links ${isOpen ? 'active scale-up-center' : ''}`}>
         <li><a href="" onClick={isOpen ? toggleNav : undefined}>Home</a></li>
         <li><a href="#about" onClick={isOpen ? toggleNav : undefined}>About</a></li>
         <li><a href="#skills" onClick={isOpen ? toggleNav : undefined}>Skills</a></li>
         <li><a href="#projects" onClick={isOpen ? toggleNav : undefined}>Projects</a></li>
         <li><a href="#contact" onClick={isOpen ? toggleNav : undefined}>Contact</a></li>
 
-        { isOpen && <SocialLinks /> }
-        { isOpen && <p className="copyright">© 2025 Anthony Chinaza. All rights reserved.</p>}
+       <SocialLinks className="show"/>
+       <p className="copyright show">© 2025 Anthony Chinaza. All rights reserved.</p>
       </ul>
   )
 }
