e665b43a882a66e5b2741367570a6c7e6ceb3b87
Codimaniac <simplyanthony007@gmail.com>
2025-11-05 11:45:42 +0100

    Perf: Load CSS Asynchronously and LCP optimization

--- src/components/ui/navlogo/NavLogo.jsx
+++ src/components/ui/navlogo/NavLogo.jsx
@@ -3,7 +3,7 @@ import Logo from '../../../assets/images/dev-naza-high-resolution-logo-transpare
 
 const NavLogo = () => {
   return (
-    <a href="" className="logo"><img src={Logo} alt='Logo' /></a>
+    <a href="" className="logo"><img src={Logo} alt='Logo' decoding="sync" loading="eager" fetchPriority="high" /></a>
   )
 }
 
